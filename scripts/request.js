e019a38fb9c996d95268ed65ef946f529f74194d
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-08-11 15:16:19 -0300

    feat: added search in the page

--- scripts/request.js
+++ scripts/request.js
@@ -1,44 +1,49 @@
 const global = document.querySelector('.global-dados');
 const brasil = document.querySelector('.brasil-dados');
+const searchByCountry = document.getElementById('s-country');
+const form = document.querySelector('form');
 
 var requestOptions = {
   method: 'GET',
-  redirect: 'follow'
+  redirect: 'follow',
 };
 
-fetch("https://api.covid19api.com/summary", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-
-    global.innerHTML = `
+function fetchData() {
+  fetch('https://api.covid19api.com/summary', requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      global.innerHTML = `
     <li><span class ="info" >Casos Confirmados:</span>${result.Global.TotalConfirmed} </li>
     <li><span class ="info" >Pacientes recuperados:</span>${result.Global.TotalRecovered}</li>
     <li><span class ="info" >Mortes:</span>${result.Global.TotalDeaths}</li>
     </ul>
     `;
-  })
-  .catch(error => console.log('error', error));
+    })
+    .catch((error) => console.log('error', error));
+}
 
+fetchData();
 
-fetch("https://api.covid19api.com/live/country/brazil/status/confirmed", requestOptions)
-  .then(response => response.json())
-  .then(result => {
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(searchByCountry.value);
+});
+
+/*
+fetch(
+  'https://api.covid19api.com/total/country/brazil/status/confirmed',
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
     const r = result[2];
-     brasil.innerHTML = `
+    brasil.innerHTML = `
     <li><span class ="info" >Casos Confirmados:</span>${r.Confirmed}</li>
     <li><span class ="info" >Pacientes recuperados:</span>${r.Recovered}</li>
     <li><span class ="info" >Mortes:</span>${r.Deaths}</li>
     </ul>
     `;
   })
-  .catch(error => console.log('error', error));
-
-for(let x=0; x<=200;x++){
-if(x%3 == 0){
-  console.log('fizz = ',x);
-}if(x%5 == 0){
-  console.log('buzz = ',x);
-}else{
-  console.log(x);
-}
-};
+  .catch((error) => console.log('error', error));*/
